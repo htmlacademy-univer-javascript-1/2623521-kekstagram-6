6c0e3415faac7d640cb8822656128684998c6ccd
DilanAguilar21 <dilanaguilar035@gmail.com>
2025-10-19 10:48:03 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 import { createPhotos } from './data.js';
+import { renderPictures } from './pictures.js';
 
 function init() {
+  // genera 25 objetos con datos temporales
   const photos = createPhotos(25);
-  console.log('Fotos generadas:', photos);
+
+  renderPictures(photos);
 }
 
 init();
