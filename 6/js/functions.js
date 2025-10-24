ba5c3c542ec347a36aec3419809e7b75ccebb110
Keks <keks@htmlacademy.ru>
2025-10-24 12:41:04 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/functions.js
@@ -0,0 +1,23 @@
+function isLengthWithin(str, max) {
+  return String(str).length <= max;
+}
+
+function isPalindrome(str) {
+  const s = String(str).toLowerCase().replace(/\s/g, '');
+  for (let i = 0, j = s.length - 1; i < j; i += 1, j -= 1) {
+    if (s[i] !== s[j]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// Opcional
+function extractNumber(value) {
+  const digits = String(value).match(/\d/g);
+  return digits ? Number(digits.join('')) : NaN;
+}
+
+window.isLengthWithin = isLengthWithin;
+window.isPalindrome  = isPalindrome;
+window.extractNumber = extractNumber;
---
+++ 6/js/main.js
@@ -0,0 +1,8 @@
+import { createPhotos } from './data.js';
+
+function init() {
+  const photos = createPhotos(25);
+  console.log('Fotos generadas:', photos);
+}
+
+init();
