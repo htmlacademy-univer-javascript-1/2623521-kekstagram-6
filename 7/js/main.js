df2b0ec1fb7b00a1454edf0568cd8bd080291b90
Keks <keks@htmlacademy.ru>
2025-12-01 09:09:46 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/main.js
@@ -0,0 +1,10 @@
+import { createPhotos } from './data.js';
+import { renderPictures } from './pictures.js';
+
+function init() {
+  const photos = createPhotos(25);
+  renderPictures(photos);
+}
+
+init();
+
